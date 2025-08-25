36b63a18d5fbf7c3328717967fa4dd4caea1ba78
Nandhini-ns <nandhininadimuthu99@gmail.com>
2025-08-25 16:47:08 +0530

    CRUD Operation in React.js

--- src/Compontent/Curd Opration/StudentTable.js
+++ src/Compontent/Curd Opration/StudentTable.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React,{ useState,useEffect,useRef } from 'react';
+import { useNavigate } from "react-router-dom";
 import './Student.css';
-import { BsEye, BsPencil, BsTrash } from "react-icons/bs";
+import { BsThreeDotsVertical, BsEye, BsPencil, BsTrash } from "react-icons/bs";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 export default function StudentTable() {
+   const [open, setOpen] = useState(false);
+   const menuRef = useRef(null);
+   const navigate = useNavigate();
+
+   useEffect(()=>{
+    const handleClickOutside = (event)=>{if(menuRef.current && !menuRef.current.contains(event.target)){
+      setOpen(false);
+    }
+  };
+     document.addEventListener("mousedown", handleClickOutside);
+      return () => document.removeEventListener("mousedown", handleClickOutside);
+   },[]);
   return (
     <div>
         <div className='container-s'>
             <h2>Student Records</h2>
             <div className='table-container'>
-                <button className='btn btn-add'>Add</button>
+                <button className='btn btn-add' onClick={() => navigate("/create")}>Add</button>
                 <table>
                     <thead>
                         <tr>
@@ -20,13 +36,14 @@ export default function StudentTable() {
                            <th>Actions</th> 
                         </tr>
                     </thead>
-                    <tbody className='tal'>
+                    {/* <tbody className='tal'>
+                      <tr>
                       <td>1</td>
                       <td>1001</td>
                       <td>Ji</td>
                       <td>Thanjavur</td>
                       <td>7865930108</td>
-                      <td>
+                      {/* <td>
                          <button className="btn-action view">
                             <BsEye /> 
                          </button>
@@ -36,8 +53,46 @@ export default function StudentTable() {
                             <button className="btn-action delete">
                                <BsTrash /> 
                             </button>
-                       </td>
-                    </tbody>
+                       </td> */}
+
+                     <tbody className='tal'>
+                          <tr>
+                              <td>1</td>
+                              <td>1001</td>
+                              <td>Ji</td>
+                              <td>Thanjavur</td>
+                              <td>7865930108</td>
+                                <td className="position-relative">
+  <div ref={menuRef} style={{ display: "inline-block", position: "relative" }}>
+    {/* 3 dots button */}
+    <button
+      className="btn btn-light btn-sm"
+      onClick={() => setOpen(!open)}
+    >
+      <BsThreeDotsVertical />
+    </button>
+
+    {/* Dropdown */}
+    {open && (
+      <div
+        className="custom-dropdown">
+        <button className="dropdown-item d-flex align-items-center">
+          <BsEye style={{ marginRight: "8px",color:"blue",width:"100px" }} />
+        </button>
+        <button className="dropdown-item d-flex align-items-center">
+          <BsPencil style={{ marginRight: "8px" }} />
+        </button>
+        <button className="dropdown-item text-danger d-flex align-items-center">
+          <BsTrash style={{ marginRight: "8px" }} /> 
+        </button>
+      </div>
+    )}
+  </div>
+</td>
+
+                              </tr>
+                       </tbody>
+
                 </table>
 
             </div>
